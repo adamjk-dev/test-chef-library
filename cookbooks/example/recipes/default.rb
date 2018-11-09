5b39e117a72ddc33f619105772a0ce1fa089bf63
Adam Kunk <adam.jk.dev@gmail.com>
2018-11-08 19:49:39 -0500

    Update default.rb

--- cookbooks/example/recipes/default.rb
+++ cookbooks/example/recipes/default.rb
@@ -6,8 +6,20 @@ log "Welcome to Chef!" do
   level :info
 end
 
-cb_found = cookbook_in_context?('example', '1.9.0')
+cb_found = cookbook_in_context?('examplenotfound', '1.0.0')
 
-log "cookbook found in context: #{cb_found}" do
+log "cookbook found in context (should not be): #{cb_found}" do
+  level :info
+end
+
+cb_found = cookbook_in_context?('example', '1.0.0')
+
+log "cookbook found in context (should be): #{cb_found}" do
+  level :info
+end
+
+cb_found = cookbook_in_context?('example', '1.0.1')
+
+log "cookbook found in context (should not be): #{cb_found}" do
   level :info
 end
